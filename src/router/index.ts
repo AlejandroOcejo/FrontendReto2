0255bee0301b43a1adaadf6f618f980e06bc57f6
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-02-08 17:57:28 +0100

    initial commit

---
+++ src/router/index.ts
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const Home = () => import('../views/MainPage.vue');
+const Registro = { template: '<div></div>' };
+const Reserva = { template: '<div></div>' };
+const InicioSesion = { template: '<div></div>' };
+
+
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/registro', component: Registro },
+  { path: '/reserva', component: Reserva },
+  { path: '/iniciosesion', component: InicioSesion },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
